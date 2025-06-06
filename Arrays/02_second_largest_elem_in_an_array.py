8b6ce99e8ebec592f9cf098be37caf3b1d47fd07
s-atharva <atharvasarbhukan@gmail.com>
2025-06-06 11:46:37 +0530

    find second largest elem in an array

--- Arrays/02_second_largest_elem_in_an_array.py
+++ Arrays/02_second_largest_elem_in_an_array.py
@@ -10,6 +10,7 @@ def find_second_largest_elem(my_array):
 
     return "Second largest element is not found"
 
+
 def find_second_largest_elem_better(my_array):
     if len(my_array) < 2:
         return "Second largest element is not found"
@@ -25,7 +26,21 @@ def find_second_largest_elem_better(my_array):
     return second_largest_element
 
 
+def find_second_largest_elem_best(my_array):
+    if len(my_array) < 2:
+        return "Second largest element is not found"
+    largest_element = my_array[0]
+    second_largest_element = float("-inf")
+    for i in range(1, len(my_array)):
+        if my_array[i] > largest_element:
+            second_largest_element = largest_element
+            largest_element = my_array[i]
+        elif my_array[i] < largest_element and my_array[i] > second_largest_element:
+            second_largest_element = my_array[i]
+    return second_largest_element
+
 
 arr = [1, 2, 4, 7, 7, 5]
 print(find_second_largest_elem(my_array=arr))
 print(find_second_largest_elem_better(my_array=arr))
+print(find_second_largest_elem_best(my_array=arr))
